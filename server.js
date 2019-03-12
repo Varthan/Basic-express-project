ecd89bb3a56a623ed0df0379063248e6ac324d21
Gowtham Varthan <36688223+Varthan@users.noreply.github.com>
2019-03-12 10:13:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ res.sendFile(path.join(__dirname,'/bank/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => console.log('Listening on http://localhost:3000'));
